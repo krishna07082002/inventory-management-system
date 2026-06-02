ff81e1aceb9845cd9cf677d24feab536fe14f96f
Krishna <krishnachaturvedi352@gmail.com>
2026-06-02 12:42:57 +0530

    final push

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,20 +1,15 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL not set")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
-
+SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
 
 
